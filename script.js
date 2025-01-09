84368bb0c8ac345f9b33a555d16aa744f969b340
Deanna Mandarino <mandarinodeanna@gmail.com>
2025-01-09 13:54:30 -0500

    Added checkSlide function and event listener

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function debounce(func, wait = 15, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.content__image');
+
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+    // Get the position of the image and its dimensions
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    // Check if the image is visible in the viewport
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    // Add or remove the 'active' class based on the image's visibility
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    }
+    else {
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+// Attach the debounced 'checkSlide' function to the scroll event
+window.addEventListener('scroll', debounce(checkSlide));
