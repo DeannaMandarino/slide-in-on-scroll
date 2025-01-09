b77ace659d61e70a54f481a52287d2bb19545156
Deanna Mandarino <mandarinodeanna@gmail.com>
2025-01-09 13:53:52 -0500

    Created script.js file and added debounce function from wesbos

---
+++ script.js
@@ -0,0 +1,15 @@
+// Debounce function given by Wesbos
+function debounce(func, wait = 15, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
